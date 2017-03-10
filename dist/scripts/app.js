13262912a3422e7c42e4c704fde74d574634d4f8
Ray Santiago <rsantiago1186@gmail.com>
2017-03-10 03:22:30 +0000

    Fixing templates

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,24 +1,24 @@
 (function() {
     function config($stateProvider, $locationProvider) {
-      $locationProvider
-     .html5Mode({
-         enabled: true,
-         requireBase: false
-     });
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
 
-     $stateProvider
-         .state('landing', {
-             url: '/',
-             templateUrl: '/templates/landing.html'
-           })
-           .state('album', {
-               url: '/album',
-               templateUrl: '/templates/album.html'
-           });
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
            .state('collection', {
-               url: '/collection',
-               templateUrl: '/templates/collection.html'
-          })
+                url: '/collection',
+                templateUrl: 'templates/collection.html'
+            });
     }
 
     angular
