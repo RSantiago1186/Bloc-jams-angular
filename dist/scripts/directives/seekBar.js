d691a21937d4a8f1e71a0fc7bfdf42423d864eb9
Ray Santiago <RSantiago1186@gmail.com>
2017-03-23 19:30:53 -0700

    All merged

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -1,108 +1,94 @@
 (function() {
-    function seekBar($document) {
+  function seekBar($document) {
 
-        /**
-         * @function calculatePercent
-         * @desc Calculates percentage of seek bar length
-         * @params {Object} seekBar, {Object} event
-         */
-        var calculatePercent = function(seekBar, event) {
-            var offsetX = event.pageX - seekBar.offset().left;
-            var seekBarWidth = seekBar.width();
-            var offsetXPercent = offsetX / seekBarWidth;
-            offsetXPercent = Math.max(0, offsetXPercent);
-            offsetXPercent = Math.min(1, offsetXPercent);
-            return offsetXPercent;
-        };
+    /**
+    * @function calculatePercent
+    * @desc Calculates percentage of seek bar length
+    * @params {Object} seekBar, {Object} event
+    */
+    var calculatePercent = function(seekBar, event) {
+      var offsetX = event.pageX - seekBar.offset().left;
+      var seekBarWidth = seekBar.width();
+      var offsetXPercent = offsetX / seekBarWidth;
+      offsetXPercent = Math.max(0, offsetXPercent);
+      offsetXPercent = Math.min(1, offsetXPercent);
+      return offsetXPercent;
+    };
 
-        return {
-            templateUrl: '/templates/directives/seek_bar.html',
-            replace: true,
-            restrict: 'E',
-            scope: {
-                onChange: '&'
-            },
-            //updates DOM
-            link: function(scope, element, attributes) {
-                // bulk of directive logic is placed here and is returned
-                scope.value = 0;
-                scope.max = 100;
+    return {
+      templateUrl: '/templates/directives/seek_bar.html',
+      replace: true,
+      restrict: 'E',
+      scope: { },
+      //updates DOM
+      link: function(scope, element, attributes) {
+      // bulk of directive logic is placed here and is returned
+      scope.value = 0;
+      scope.max = 100;
 
-                var seekBar = $(element);
+      var seekBar = $(element);
 
-                attributes.$observe('value', function(newValue) {
-                    scope.value = newValue;
-                });
+      /**
+      * @function percentString
+      * @desc Converts percentage number to string type
+      */
+      var percentString = function () {
+        var value = scope.value;
+        var max = scope.max;
+        var percent = value / max * 100;
+        return percent + "%";
+      };
 
-                attributes.$observe('max', function(newValue) {
-                    scope.max = newValue;
-                });
+      /**
+      * @function fillStyle
+      * @desc Returns width of bar based on percent
+      */
+      scope.fillStyle = function() {
+        return {width: percentString()};
+      };
 
-                /**
-                 * @function percentString
-                 * @desc Converts percentage number to string type
-                 */
-                var percentString = function() {
-                    var value = scope.value;
-                    var max = scope.max;
-                    var percent = value / max * 100;
-                    return percent + "%";
-                };
+      /**
+      * @function thumbStyle
+      * @desc Updates position of thumb
+      */
+      scope.thumbStyle = function() {
+        return {left: percentString()};
+      };
 
-                /**
-                 * @function fillStyle
-                 * @desc Returns width of bar based on percent
-                 */
-                scope.fillStyle = function() {
-                    return {
-                        width: percentString()
-                    };
-                };
+      /**
+      * @function onClickSeekBar
+      * @desc Updates seek bar value based on bar width and location of click
+      */
+      scope.onClickSeekBar = function(event) {
+        var percent = calculatePercent(seekBar, event);
+        scope.value = percent * scope.max;
+      };
 
-                /**
-                 * @function thumbStyle
-                 * @desc Updates position of thumb
-                 */
-                scope.thumbStyle = function() {
-                    return {
-                        left: percentString()
-                    };
-                };
+      /**
+      * @function trackThumb
+      * @desc Continuously applies change to seek bar value when thumb is dragged
+      */
+      scope.trackThumb = function() {
+        $document.bind('mousemove.thumb', function(event) {
+          var percent = calculatePercent(seekBar, event);
+          scope.$apply(function() {
+            scope.value = percent * scope.max;
+          });
+        });
 
-                /**
-                 * @function onClickSeekBar
-                 * @desc Updates seek bar value based on bar width and location of click
-                 */
-                scope.onClickSeekBar = function(event) {
-                    var percent = calculatePercent(seekBar, event);
-                    scope.value = percent * scope.max;
-                };
+        $document.bind('mouseup.thumb', function() {
+          $document.unbind('mousemove.thumb');
+          $document.unbind('mouseup.thumb');
+        });
 
-                /**
-                 * @function trackThumb
-                 * @desc Continuously applies change to seek bar value when thumb is dragged
-                 */
-                scope.trackThumb = function() {
-                    $document.bind('mousemove.thumb', function(event) {
-                        var percent = calculatePercent(seekBar, event);
-                        scope.$apply(function() {
-                            scope.value = percent * scope.max;
-                        });
-                    });
+      }; // end of trackThumb method
 
-                    $document.bind('mouseup.thumb', function() {
-                        $document.unbind('mousemove.thumb');
-                        $document.unbind('mouseup.thumb');
-                    });
+      } // end of link method
+    };
 
-                }; // end of trackThumb method
+  }
 
-            } // end of link method
-        };
-
-    }
-
-    angular
-        .module('blocJams')
-        .directive('seekBar', ['$document', seekBar]);
+  angular
+    .module('blocJams')
+    .directive('seekBar', ['$document', seekBar]);
 })();
